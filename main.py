dd3bb6dc9ee9d660b2af0d324ac7b1f87720bb1e
DanSoW <swdaniel@yandex.ru>
2024-12-27 10:36:30 +0000

    feat update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from transformers import AutoTokenizer, AutoModelForCausalLM
 import torch
 import nvmem
 import gc
+import sys
 
 torch.cuda.empty_cache()
 
@@ -13,19 +14,22 @@ model_name = "microsoft/Phi-3-mini-4k-instruct"
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 tokenizer = AutoTokenizer.from_pretrained(model_name)
-model = AutoModelForCausalLM.from_pretrained(model_name, device_map="cuda")
+model = AutoModelForCausalLM.from_pretrained(model_name, device_map="auto")
 
 nvmem.printMemoryUsed()
 
+#sys.exit()
+
 input_text = "write bubble sort on python"
-model_inputs = tokenizer.encode(input_text, return_tensors="pt").to(device)
+model_inputs = tokenizer.encode(input_text, return_tensors="pt")
+model_inputs = model_inputs.cuda()
 
 torch.cuda.empty_cache()
 gc.collect()
 
 nvmem.printMemoryUsed()
 
-out = model.generate(model_inputs, max_new_tokens=512)[0]
+out = model.generate(model_inputs, max_length=1024, do_sample=True)[0]
 
 generated_text = list(map(tokenizer.decode, out))[0]
 result = str(generated_text).removeprefix(input_text)
