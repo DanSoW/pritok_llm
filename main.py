88c87a34aa8f77e21b8f2d91e6c80a82e26afcc3
DanSoW <swdaniel@yandex.ru>
2025-01-06 11:12:51 +0000

    fix

--- main.py
+++ main.py
@@ -9,32 +9,36 @@ torch.cuda.empty_cache()
 nvmem.printInfoCUDA()
 nvmem.printMemoryUsed()
 
-model_name = "microsoft/Phi-3-mini-4k-instruct"
+#model_name = "microsoft/Phi-3-mini-4k-instruct"
+model_name = "ai-forever/rugpt3small_based_on_gpt2"
+#model_name = "Qwen/Qwen2.5-Coder-3B"
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 tokenizer = AutoTokenizer.from_pretrained(model_name)
-model = AutoModelForCausalLM.from_pretrained(model_name, device_map="auto")
+model = AutoModelForCausalLM.from_pretrained(model_name).cuda()
 
 nvmem.printMemoryUsed()
 
 #sys.exit()
 
-input_text = "write bubble sort on python"
+input_text = "Как твои дела?"
 model_inputs = tokenizer.encode(input_text, return_tensors="pt")
 model_inputs = model_inputs.cuda()
 
-torch.cuda.empty_cache()
-gc.collect()
+#torch.cuda.empty_cache()
+#gc.collect()
 
 nvmem.printMemoryUsed()
 
-out = model.generate(model_inputs, max_length=1024, do_sample=True)[0]
+
+model.generation_config.pad_token_id = tokenizer.pad_token_id
+out = model.generate(model_inputs, max_new_tokens=2048,  max_length=1024, do_sample=True)[0]
 
 generated_text = list(map(tokenizer.decode, out))[0]
-result = str(generated_text).removeprefix(input_text)
+#result = str(generated_text).removeprefix(input_text)
 
-print(result)
+print(generated_text)
 
 
 
